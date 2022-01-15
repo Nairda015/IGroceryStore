80afa6f2ea95038657a8b5b7af8f98c943871e00
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2022-01-15 12:49:30 +0100

    Add Store.cs

--- Shops/Stores.Core/Entities/Store.cs
+++ Shops/Stores.Core/Entities/Store.cs
@@ -1,9 +1,8 @@
-﻿namespace IGroceryStore.Shops.Core.Entities;
+﻿namespace IGroceryStore.Stores.Core.Entities;
 
-public class Shop
+public class Store
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Address { get; set; }
-    
 }
