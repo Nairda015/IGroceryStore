7588bf084bc887539183f3275f85978db51a6c06
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2022-07-31 11:36:48 +0200

    remove unused variable

--- src/Shared/Shared/Configuration/AppInitializer.cs
+++ src/Shared/Shared/Configuration/AppInitializer.cs
@@ -10,33 +10,24 @@ public static class AppInitializer
     private const string ModulePrefix = "IGroceryStore";
     public static AppContext Initialize(WebApplicationBuilder builder)
     {
-        var disabledModules = new HashSet<string>();
         var assemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
         var locations = assemblies.Where(x => !x.IsDynamic).Select(x => x.Location).ToArray();
         var files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll")
             .Where(x => !locations.Contains(x, StringComparer.InvariantCultureIgnoreCase))
             .ToList();
 
-        var groceryStoreFiles = files
-            .Select(x => x.Split(ModulePrefix, 2).Last())
-            .Where(x => x.Contains(ModulePrefix))
-            .ToList();
-        
         foreach (var file in files)
         {
             var root = file.Split(ModulePrefix, 2).Last();
             if (!root.Contains(ModulePrefix)) continue;
 
-            var moduleName = root.Split($"{ModulePrefix}")[1].Split(".", StringSplitOptions.RemoveEmptyEntries)[0];
+            var moduleName = root.Split($"{ModulePrefix}")[1]
+                .Split(".", StringSplitOptions.RemoveEmptyEntries)[0];
             var enabled = builder.Configuration.GetValue<bool>($"{moduleName}:ModuleEnabled");
             
             if (enabled)
             {
                 assemblies.Add(AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(file)));
-            }
-            else
-            {
-                disabledModules.Add(moduleName);
             }
         }
 
