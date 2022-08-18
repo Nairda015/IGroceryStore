f20446d7d5dae97bfb3f3fefc71466cffb6842ff
Abachaczi <marcinkanardev@gmail.com>
2022-08-18 22:48:38 +0200

    Add delete, update, and add operations to category and add map delete extension for command.

---
+++ src/Products/Products.Core/Features/Categories/Commands/UpdateCategory.cs
@@ -0,0 +1,47 @@
+﻿using IGroceryStore.Products.Core.Exceptions;
+using IGroceryStore.Products.Core.Persistence.Contexts;
+using IGroceryStore.Shared.Abstraction.Commands;
+using IGroceryStore.Shared.Abstraction.Common;
+using IGroceryStore.Shared.Abstraction.Constants;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+
+namespace IGroceryStore.Products.Core.Features.Categories.Commands;
+
+internal record UpdateCategory(UpdateCategory.UpdateCategoryBody Body, ulong Id) : IHttpCommand
+{
+    internal record UpdateCategoryBody(ulong CategoryId, string Name);
+}
+
+public class UpdateCategoryEndpoint : IEndpoint
+{
+    public void RegisterEndpoint(IEndpointRouteBuilder endpoints) =>
+        endpoints.MapPut<UpdateCategory>("category/{id}")
+            .WithTags(SwaggerTags.Products)
+            .Produces(200)
+            .Produces(400);
+}
+
+internal class UpdateCategoryHandler : ICommandHandler<UpdateCategory, IResult>
+{
+    private readonly ProductsDbContext _productsDbContext;
+
+    public UpdateCategoryHandler(ProductsDbContext productsDbContext)
+    {
+        _productsDbContext = productsDbContext;
+    }
+
+    public async Task<IResult> HandleAsync(UpdateCategory command, CancellationToken cancellationToken = default)
+    {
+        var (categoryId, name) = command.Body;
+        var category =
+            await _productsDbContext.Categories.FirstOrDefaultAsync(x => x.Id.Equals(categoryId), cancellationToken);
+
+        if (category is null) throw new CategoryNotFoundException(categoryId);
+
+        _productsDbContext.Update(category);
+        await _productsDbContext.SaveChangesAsync(cancellationToken);
+        return Results.Ok();
+    }
+}
