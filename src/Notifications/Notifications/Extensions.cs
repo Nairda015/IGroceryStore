95f5c8d7a2c3c52c19b5e8c051be3687e19071fd
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2022-08-19 22:31:10 +0200

    Add notification module

---
+++ src/Notifications/Notifications/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using IGroceryStore.Shared.Abstraction.Common;
+using IGroceryStore.Shared.Abstraction.Constants;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Notifications;
+
+public class NotificationsModule : IModule
+{
+    public string Name => "Notifications";
+
+    public void Register(IServiceCollection services, IConfiguration configuration)
+    {
+
+    }
+
+    public void Use(IApplicationBuilder app)
+    {
+    }
+
+    public void Expose(IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet($"/api/{Name.ToLower()}/health", () => $"{Name} module is healthy")
+            .WithTags(SwaggerTags.HealthChecks);
+
+        var assembly = Assembly.GetAssembly(typeof(NotificationsModule));
+        var moduleEndpoints = assembly!
+            .GetTypes()
+            .Where(x => typeof(IEndpoint).IsAssignableFrom(x) && x.IsClass)
+            .OrderBy(x => x.Name)
+            .Select(Activator.CreateInstance)
+            .Cast<IEndpoint>()
+            .ToList();
+
+        moduleEndpoints.ForEach(x => x.RegisterEndpoint(endpoints));
+    }
+}
