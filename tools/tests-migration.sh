a0f2726a1e46a2c269ccf63d78692e9a6bcf6769
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2022-08-24 16:49:02 +0200

    simplify gh build

--- tools/tests-migration.sh
+++ tools/tests-migration.sh
@@ -7,7 +7,7 @@ for moduleName in "${modules[@]}" ; do
     cd "$moduleName" || exit
     cd "$moduleName".Core || exit
     dotnet ef migrations add "test-migration" -o Persistence/Migrations -s ../../API -c "$moduleName"DbContext
-    dotnet ef database update -s ../../API -c "$moduleName"DbContext
+    #dotnet ef database update -s ../../API -c "$moduleName"DbContext
     cd ../..
 done
 
