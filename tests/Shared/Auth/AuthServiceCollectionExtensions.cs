4efcf6260b4db62e0875fa638a22e52fce0cb43d
GrzegorzFedoryka <gfedoryka@gmail.com>
2022-10-26 22:47:12 +0200

    Added auth base

---
+++ tests/Shared/Auth/AuthServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Shared.Auth
+{
+    public static class AuthServiceCollectionExtensions
+    {
+        public static AuthenticationBuilder AddTestAuthentication(this IServiceCollection services)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.DefaultPolicy = new AuthorizationPolicyBuilder(AuthConstants.Scheme)
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
+
+            return services.AddAuthentication(AuthConstants.Scheme)
+                .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(AuthConstants.Scheme, options => { });
+        }
+    }
+}
