c3be7ce7547dbaa17e0dcf9c9079b747383a8fa4
Robertdz2001 <robertdz0@onet.pl>
2022-10-30 21:17:44 +0100

    Add create, update, delete operations for brands

---
+++ src/Products/Products.Core/Exceptions/BrandNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using IGroceryStore.Products.Core.ValueObjects;
+using IGroceryStore.Shared.Abstraction.Exceptions;
+
+namespace IGroceryStore.Products.Core.Exceptions
+{
+    internal class BrandNotFoundException : GroceryStoreException
+    {
+        public override HttpStatusCode StatusCode => HttpStatusCode.NotFound;
+
+        public BrandNotFoundException(BrandId message) : base($"Brand with id {message} not found")
+        {
+        }
+    }
+}
---
+++ src/Products/Products.Core/Features/Brands/Commands/AddBrand.cs
@@ -0,0 +1,50 @@
+﻿using IGroceryStore.Products.Core.Entities;
+using IGroceryStore.Products.Core.Persistence.Contexts;
+using IGroceryStore.Shared.Abstraction.Commands;
+using IGroceryStore.Shared.Abstraction.Common;
+using IGroceryStore.Shared.Abstraction.Constants;
+using IGroceryStore.Shared.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace IGroceryStore.Products.Core.Features.Brands.Commands
+{
+    internal record AddBrand(AddBrand.AddBrandBody Body) : IHttpCommand
+    {
+        internal record AddBrandBody(string Name);
+    }
+
+    public class AddBrandEndpoint : IEndpoint
+    {
+        public void RegisterEndpoint(IEndpointRouteBuilder endpoints) =>
+        endpoints.MapPost<AddBrand>("api/brands")
+            .WithTags(SwaggerTags.Products)
+            .Produces(200);
+    }
+
+    internal class AddBrandHandler : ICommandHandler<AddBrand, IResult>
+    {
+        private readonly ProductsDbContext _productsDbContext;
+        private readonly ISnowflakeService _snowflakeService;
+
+        public AddBrandHandler(ProductsDbContext productsDbContext, ISnowflakeService snowflakeService)
+        {
+            _productsDbContext = productsDbContext;
+            _snowflakeService = snowflakeService;
+        }
+
+        public async Task<IResult> HandleAsync(AddBrand command, CancellationToken cancellationToken = default)
+        {
+            var brand = new Brand
+            {
+                Id = _snowflakeService.GenerateId(),
+                Name = command.Body.Name
+            };
+
+            _productsDbContext.Brands.Add(brand);
+            await _productsDbContext.SaveChangesAsync(cancellationToken);
+            return Results.Ok(brand.Id);
+        }
+    }
+
+}
