187a07432bd8fff01da286d870993d632b207992
GrzegorzFedoryka <gfedoryka@gmail.com>
2023-01-09 23:06:20 +0100

    added allergens collection as a value object

--- src/Products/Products.Core/Entities/Product.cs
+++ src/Products/Products.Core/Entities/Product.cs
@@ -1,4 +1,6 @@
-﻿using IGroceryStore.Products.ValueObjects;
+﻿using System.Collections;
+using System.Collections.Immutable;
+using IGroceryStore.Products.ValueObjects;
 using IGroceryStore.Shared.Common;
 using IGroceryStore.Shared.ValueObjects;
 
@@ -20,7 +22,11 @@ internal class Product : AuditableEntity
     public Brand Brand { get; set; }
     public CategoryId CategoryId { get; set; }
     public Category Category { get; set; }
-    public HashSet<Allergen> Allergens { get; private set; } = new();
+    public AllergenCollection Allergens { get; set; }
+
+    internal Product()
+    {
+    }
     
     public void MarkAsObsolete()
     {
@@ -29,7 +35,61 @@ internal class Product : AuditableEntity
     
     public void AddAllergen(Allergen allergen)
     {
-        if (Allergens.Contains(allergen)) return;
         Allergens.Add(allergen);
+    }
+
+    public sealed class AllergenCollection : IEnumerable<Allergen>, IEquatable<AllergenCollection>
+    {
+        private ISet<Allergen> _allergens;
+        public static AllergenCollection Empty => new(Enumerable.Empty<Allergen>());
+
+        public IReadOnlySet<Allergen> Allergens
+        {
+            get { return (IReadOnlySet<Allergen>)_allergens; }
+            private set { _allergens = (ISet<Allergen>)value; }
+        }
+
+        public AllergenCollection(IEnumerable<Allergen> allergens)
+        {
+            _allergens = allergens.ToHashSet();
+        }
+
+        public bool Equals(AllergenCollection? other)
+        {
+            if (other == null) return false;
+
+            return _allergens.SetEquals(other!._allergens);
+        }
+
+        public override bool Equals(object? other)
+        {
+            return Equals(other as AllergenCollection);
+        }
+        public IEnumerator<Allergen> GetEnumerator()
+        {
+            return _allergens.GetEnumerator();
+        }
+        public override int GetHashCode()
+        {
+            return _allergens.Select(x => x != null ? x.GetHashCode() : 0).Aggregate((x, y) => x ^ y);
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public static bool operator == (AllergenCollection? left, AllergenCollection? right)
+        {
+            return left != null && left.Equals(right);
+        }
+        public static bool operator !=(AllergenCollection? left, AllergenCollection? right)
+        {
+            return left == null || !left.Equals(right);
+        }
+        public void Add(Allergen allergen)
+        {
+            var allergens = Allergens.ToHashSet();
+            allergens.Add(allergen);
+            Allergens = allergens;
+        }
     }
 }
